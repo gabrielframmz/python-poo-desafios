ad1d26f171416e9533ce003cf46865f750bdcb0f
Gabriel G. <gabrielsolarjacutinga@outlook.com>
2026-05-03 16:47:14 -0300

    password generator: first working version

--- outros/password generator/__main__.py
+++ outros/password generator/__main__.py
@@ -4,13 +4,29 @@ from module import password as pw
 def main() -> None:
     print(f'{"_*_" * 5}PASSWORD GENERATOR{"_*_" * 5}')
 
-    try:
-        length = int(input('Número de caracteres: '))
-    except ValueError:
-        length = 8
+    while True:
+        try:
+            print('_' * 48)
+            length = int(input('\033[0;34mNúmero de caracteres:\033[m '))
+        except ValueError:
+            password = pw.generate()
+        else:
+            password = pw.generate(length)
 
-    password = pw.generate(length)
-    print(password)
+        save = input(f'\033[0;33m{password}\033[m \nSalvar? (y/n)\n').lower()
+        match save:
+            case 'y':
+                tag = input('\033[0;35mEtiqueta/Nome da senha:\033[m ')
+                pw.store(password, tag)
+            case _:
+                pass
+
+        end = input(f'Fechar programa? (y/n)\n').lower()
+        match end:
+            case 'y':
+                break
+            case _:
+                pass
 
 
 if __name__ == '__main__':
--- outros/password generator/module/password.py
+++ outros/password generator/module/password.py
@@ -1,11 +1,32 @@
-from random import randint, choice
+import secrets
+import string
+from os import makedirs
 
 
 def generate(length: int = 8) -> str:
+    potential = string.ascii_letters + string.digits + '.<>/?^~-_=+*&%$#@!|'
     password = ''
     for _ in range(0, length):
-        number = str(randint(0, 9))
-
-        char = choice([number])
+        char = secrets.choice(potential)
         password += char
     return password
+
+
+def store(password: str, tag: str) -> None:
+    makedirs(f'./Passwords/', exist_ok=True)
+    path = f'./Passwords/passwords.txt'
+
+    try:
+        open(path, 'r')
+    except FileNotFoundError:
+        f = open(path, 'w')
+        f.write('DO NOT SHARE THIS FILE! '
+                'IT IS RECOMMENDED TO ONLY USE THIS FILE TEMPORALLY UNTIL YOU SAVE YOUR PASSWORDS SOMEWHERE SAFER.'
+                '\n')
+        f.close()
+    finally:
+        f = open(path, 'a')
+        f.write(f'\n{tag}: {password}')
+        f.close()
+
+    print('\033[0;33mSenha salva!!!\033[m')
